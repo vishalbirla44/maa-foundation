a32b44127f91222cd5c7dfa9121c1a538d11781b
Anshul <ananyaj138@gmail.com>
2024-06-11 21:12:00 +0530

    " heading commit "

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -40,10 +40,9 @@ export default function Contact() {
           <div className="flex flex-wrap justify-center items-center w-full lg:w-1/2">
             <div className="w-full p-4">
               <div>
-                <div className="text-2xl font-normal leading-9 text-red-600">
-                  MAA FOUNDATION
-                </div>
                 <p className="text-2xl font-normal leading-9 text-zinc-900 mt-2">
+                  <span className="text-2xl font-normal leading-9 text-red-600">
+                  MAA FOUNDATION  </span>
                   has been working in India for over 70 years, for the
                   upliftment of marginalised women and girls through 53
                   programmes in 18 states and 130+ districts.
